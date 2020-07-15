dd9102e4c7570aec78a1cceafcf47a4c05371836
David Gibbons <gibbonsdav@gmail.com>
2020-07-15 15:54:07 -0700

    more validation

--- script.js
+++ script.js
@@ -27,11 +27,33 @@ function showSuccess(input) {
 function checkRequired(inputArr) {
   inputArr.forEach(function (input) {
     if (input.value.trim() === "") {
-      showError(input, `${input.id} is required`)
+      showError(input, `${getFieldName(input)} is required`)
     } else {
       showSuccess(input)
     }
   })
+}
+
+// check input length
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    )
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters`
+    )
+  } else {
+    showSuccess()
+  }
+}
+
+//get fieldname
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1)
 }
 
 // Event Listeners
@@ -39,4 +61,6 @@ form.addEventListener("submit", function (e) {
   e.preventDefault()
 
   checkRequired([username, email, password, password2])
+  checkLength(username, 3, 15)
+  checkLength(password, 6, 25)
 })
